2def726c10ab34592d79049c61048de907c43e89
HanVbear <ryoitenkaii@gmail.com>
2025-11-26 08:06:23 +0700

    adjust some pict and responsive css

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// buat animasi typing
+let typed = new Typed(".typing", {
+    strings:["Front-End Dev","Web Developer", "Mobile Developer", "Web Designer"],
+    typeSpeed:60,
+    backSpeed:50,
+    loop:true
+})
+
+
+//buat nagivasi aside
+
+const aside = document.querySelector(".aside-content");
+const toggler = document.querySelector(".nav-toggler");
+
+toggler.addEventListener("click", () => {
+    aside.classList.toggle("open");
+    aside.classList.toggle("close");
+});
+
+const navItems = document.querySelectorAll(".nav li a");
+
+navItems.forEach((item) => {
+    item.addEventListener("click", function () {
+
+        navItems.forEach((nav) => nav.classList.remove("active"));
+
+        this.classList.add("active");
+
+        if (window.innerWidth <= 991) {
+            aside.classList.remove("open");
+            aside.classList.add("close");
+        }
+    });
+});
+
+//buat buka gmaill
+
+document.getElementById("sendMail").addEventListener("click", () => {
+    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=hanifwalian@gmail.com", "_blank");
+});
---
+++ js/style-switcher.js
@@ -0,0 +1,22 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyle.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
